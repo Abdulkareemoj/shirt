994bd1485640d60a5d0a7713bbf8aa4a5e6a303e
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-04-19 13:42:52 +0100

    feat: :sparkles: added main pages

---
+++ src/app/customizer/page.tsx
@@ -0,0 +1,9 @@
+import ShirtCustomizer from "~/components/shirt-customizer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <ShirtCustomizer />
+    </main>
+  );
+}
