dccb8cfdf3cf50eac88d6c426eba9ec45367981d
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-04-21 15:45:41 +0100

    chore: added client pages and dashboard

---
+++ src/app/(client-area)/layout.tsx
@@ -0,0 +1,29 @@
+import "~/styles/globals.css";
+
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import { ThemeProvider } from "~/components/theme-provider";
+
+export const metadata: Metadata = {
+  title: "3D Shirt Customizer",
+  description:
+    "Design custom shirts in stunning 3D with our interactive customizer tool",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function ShirtLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        {children}
+      </ThemeProvider>
+    </div>
+  );
+}
