bcf98a193f12d1e6253aab415e81c045b819aa44
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-09-17 11:52:00 +0100

    font loader

---
+++ components/canvas/font-loader.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+
+interface FontLoaderComponentProps {
+  fontUrl: string;
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  onLoad?: (font: any) => void;
+}
+
+export default function FontLoaderComponent({
+  fontUrl,
+  onLoad,
+}: FontLoaderComponentProps) {
+  useEffect(() => {
+    const loader = new FontLoader();
+    loader.load(
+      fontUrl,
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      (font: any) => {
+        console.log("Font loaded:", font);
+        onLoad?.(font); // Call callback if needed
+      },
+      undefined,
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      (err: any) => {
+        console.error("Error loading font", err);
+      }
+    );
+  }, [fontUrl, onLoad]);
+
+  return null;
+}
