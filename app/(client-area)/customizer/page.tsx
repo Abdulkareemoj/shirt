27059a52d4e2e8789b167e516077ab2915abead9
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-09-17 11:48:47 +0100

    customizer page

---
+++ app/(client-area)/customizer/page.tsx
@@ -0,0 +1,9 @@
+import ShirtCustomizer from "~/components/canvas/shirt-customizer";
+
+export default function CustomizePage() {
+  return (
+    <main className="min-h-screen">
+      <ShirtCustomizer />
+    </main>
+  );
+}
